76ffc721e28c2f44ea20c92992c8feb46edf8ed7
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-09-26 07:08:27 +0300

    updated componets

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
+function TransactionsList (props){
+
+  let array = props.transactions.map(transactionObj => {
+    return <Transaction 
+            key={transactionObj.id} 
+            transaction={transactionObj} 
+          />
+  })
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +27,7 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+        {array}
       </tbody>
     </table>
   );
